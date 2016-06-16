c9b82f3acb2fbeb354463605de4a9ae8bc34f7d0
owen flood <oneflood@owens-MacBook-Pro.local>
2016-06-16 11:45:46 -0700

    create consultation controller, generate mailer, set up app keys

---
+++ app/mailers/consultation_mailer.rb
@@ -0,0 +1,7 @@
+class ConsultationMailer < ApplicationMailer
+  def new_consultation(consultation)
+    byebug
+    # @answer   = answer
+    # mail(to: @owner.email, subject: "You got a new answer!")
+  end
+end
